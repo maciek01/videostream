f855f640879e66880d3f68f1d80263b2ed47e994
maciek01 <maciek@kolesnik.org>
2019-12-14 15:32:09 +0000

    support dash format

---
+++ bin/stream-dash.sh
@@ -0,0 +1,3 @@
+
+nohup raspivid -o - -t 0 -vf -hf -w 1080 -h 768 -fps 10 -b 1000000 | ffmpeg -f h264 -thread_queue_size 256 -i - -vcodec copy -acodec aac -ab 128k -g 50 -b:v 1M -r 10 -strict experimental -f flv rtmp://home.kolesnik.org/dash/stream  &
+
