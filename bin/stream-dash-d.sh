0a923da36d626b34141674fc831d060e78d3e406
maciek01 <maciek@kolesnik.org>
2019-12-31 13:55:50 +0000

    streaming support

---
+++ bin/stream-dash-d.sh
@@ -0,0 +1,10 @@
+
+while [ ! -f /home/pi/modemup ]
+do
+    sleep 1
+done
+
+
+#nohup raspivid -o - -t 0 -vf -hf -w 1080 -h 768 -fps 10 -b 1000000 | ffmpeg -f h264 -thread_queue_size 256 -i - -vcodec copy -acodec aac -ab 128k -g 50 -b:v 1M -r 10 -strict experimental -f flv rtmp://home.kolesnik.org/dash/stream  &
+
+raspivid -o - -t 0 -vf -hf -w 640 -h 480 -fps 10  | ffmpeg -f h264 -thread_queue_size 256 -i - -vcodec copy -an -f flv rtmp://home.kolesnik.org/dash/stream &
